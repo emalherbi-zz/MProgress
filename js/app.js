d46e1d70ce924409bc00a114886da561120df95f
Eduardo Malherbi <emalherbi@gmail.com>
2014-12-21 23:36:53 -0200

    auto-generated commit

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ $( document ).ready(function() {
   $('#example3').click(function(e) {
     MProgress.configure({
       title: 'Example Inc...',
-      progressInc : 2,
+      progressInc : 10,
       progressUpdate : 1,
       progressTimeout : 30,
     });
@@ -53,6 +53,34 @@ $( document ).ready(function() {
     });
 
     MProgress.start();
+    window.setTimeout(function() {
+      MProgress.done();
+    }, 10000);
+  });
+
+  $('#example5').click(function(e) {
+    MProgress.configure({
+      title: 'Example Timeout...',
+      progressInc : 5,
+      progressUpdate : 1,
+      progressTimeout : 30,
+    });
+
+    MProgress.start();
+  });
+
+  $('#example6').click(function(e) {
+    MProgress.configure({
+      title: 'Example Add Event Done...',
+      progressInc : 5,
+      progressUpdate : 1,
+      progressTimeout : 30,
+    });
+
+    MProgress.start();
+    MProgress.eventDone(function() {
+      alert( 'Event Done !!!' );
+    });
     window.setTimeout(function() {
       MProgress.done();
     }, 5000);
